71f378772fb3b5967b0c61d462e65fc1094d4d96
Nikolay <nikist97@abv.bg>
2017-06-23 21:04:03 +0100

    added a MinHeap

--- ADTs/AbstractDataStructures.py
+++ ADTs/AbstractDataStructures.py
@@ -1,3 +1,6 @@
+from abc import ABC, abstractmethod
+
+
 # Abstract Data Type stack, Last In First Out (LIFO)
 class Stack(object):
 
@@ -28,20 +31,18 @@ class Stack(object):
     def type(self):
         return self.__elementsType
 
-    # the push method, which pushes an item into the stack
+    # the push method, which pushes an item into the stack, raises a TypeError if elementType is not None,
+    # but different from the parameter item's type
     def push(self, item):
         if self.__elementsType is None:
             self.__elements.append(item)
-
-        # if the elementsType attribute is different than None,
-        # check if the type of the new item is the required one; if it's not raise a TypeError
         else:
             if type(item) == self.__elementsType:
                 self.__elements.append(item)
             else:
-                raise TypeError("The element you are trying to push is not of type " + self.__elementsType)
+                raise TypeError("The element you are trying to push is not of type " + str(self.__elementsType))
 
-    # the pop method, which takes out the last element that got in the stack;
+    # the pop method, which takes out the last element that got into the stack;
     # it raises a ValueError if there is no element to pop (if size of the stack is 0)
     def pop(self):
         if len(self.__elements) > 0:
@@ -49,7 +50,7 @@ class Stack(object):
         else:
             raise ValueError("There are no elements in the stack")
 
-    # the peek method, which returns the last element that got in the stack, but doesn't remove it from the stack;
+    # the peek method, which returns the last element that got into the stack, but doesn't remove it from the stack;
     # it returns None if there is no element to peek at
     def peek(self):
         if len(self.__elements) > 0:
@@ -88,18 +89,16 @@ class Queue(object):
     def type(self):
         return self.__elementsType
 
-    # the enqueue method, which inserts an item into the queue
+    # the enqueue method, which inserts an item into the queue, raises a TypeError if elementsType is not None,
+    # but is different than the parameter item's type
     def enqueue(self, item):
         if self.__elementsType is None:
             self.__elements.append(item)
-
-        # if the elementsType attribute is different than None,
-        # check if the type of the new item is the required one; if it's not raise a TypeError
         else:
             if type(item) == self.__elementsType:
                 self.__elements.append(item)
             else:
-                raise TypeError("The element you are trying to push is not of type " + self.__elementsType)
+                raise TypeError("The element you are trying to push is not of type " + str(self.__elementsType))
 
     # the dequeue method, which removes the item that got first in the queue
     # it raises a ValueError if there is no element to dequeue(if size of the queue is 0)
@@ -121,42 +120,39 @@ class Queue(object):
             return None
 
 
-# Abstract Data Type Binary Tree
+# Abstract Data Type Binary Search Tree
 class BinarySearchTree(object):
 
     # the constructor for the binary tree, it has a root as an optional argument, if the root is specified the tree
-    # is initialised with a root, else it is initialized with no root, the elements of the tree must be from the type
+    # is initialised with a root, else it is initialized with no root, the elements in the tree must be from the type
     # specified in the constructor, the default type that is used is int
-    def __init__(self, root=None, elements_type=int):
-        # variables used when using the iterator of the binary tree
+    def __init__(self, root=None, elements_type=None):
+        # variables used for the iterator of the binary tree
         self.__successor = None
         self.__iterator_limit = None
         self.__iterator_finished = None
         self.__current_item = None
 
-        # initialising the type of the elements in the tree
+        if elements_type is None:
+            elements_type = int
+        # initialising the binary search tree
         self.__elements_type = elements_type
 
-        # if the value of the root is not specified, create a tree with None root and no elements
         if root is None:
             self.__root = root
             self.__number_of_items = 0
-        # else if the value of the root is specified create a tree with a root : Node with the value of the argument
         elif root is not None and type(root) == elements_type:
             self.__root = Node(root)
             self.__number_of_items = 1
-        # if the type of the root is not from the specified type, raise a TypeError
         else:
             raise TypeError("The binary tree can contain only elements of type " + str(elements_type) + ".")
 
     # overriding the __contains__ method
     def __contains__(self, item):
-        # calling the contains method of the binary tree
         return self.contains(item)
 
     # overriding the __len__ method
     def __len__(self):
-        # calling the get_number_of_elements method of the binary tree
         return self.get_number_of_elements()
 
     # overriding the __iter__ method, so that we can go through the elements of the tree
@@ -169,7 +165,6 @@ class BinarySearchTree(object):
 
     # overriding the __next__ method, so that we can use the iterator of the binary tree
     def __next__(self):
-        # if the iteration is over, raise StopIteration
         if self.__iterator_finished:
             raise StopIteration
 
@@ -203,44 +198,30 @@ class BinarySearchTree(object):
 
                 return self.__current_item.get_value()
 
-    # the add method, which adds a Node with the value given as an argument to the binary tree
+    # the add method, which adds a Node with the value given as an argument to the binary search tree
     def add(self, value):
-        # check for the type of the value
         if type(value) == self.__elements_type:
-            # if root is empty, add the element to the root
             if self.__root is None or self.__root.get_value() is None:
                 self.__root = Node(value)
                 self.__number_of_items = 1
-
-            # else call the Node add method
             else:
                 if self.__root.add(Node(value)):
                     self.__number_of_items += 1
-
-        # if the type of the argument is not int, raise a TypeError
         else:
             raise TypeError("The binary tree can contain only elements of type " + str(self.__elements_type) + ".")
 
     # the contains method, which finds if an element exists in the binary tree
     def contains(self, value):
-        # check if the argument's type is the specified elements' type
         if type(value) == self.__elements_type:
-            # call the Node contains method
             return self.__root.contains(value)
-
-        # if the type of the argument is not from the specified type, raise a TypeError
         else:
             raise TypeError("The binary tree can contain only elements of type " + str(self.__elements_type) + ".")
 
     # the delete method, which deletes an element from the tree
     def delete(self, value):
-        # check if the argument's type is specified elements' type
         if type(value) == self.__elements_type:
-            # calling the Node delete method
             self.__root.delete(value)
             self.__number_of_items -= 1
-
-        # if the type of the argument is not from the specified type, raise a TypeError
         else:
             raise TypeError("The binary tree can contain only elements of type " + str(self.__elements_type) + ".")
 
@@ -263,7 +244,6 @@ class BinarySearchTree(object):
     def get_minimum(self):
         current_node = self.__root
 
-        # if the root is None, return None
         if current_node is None:
             return None
 
@@ -272,11 +252,11 @@ class BinarySearchTree(object):
             current_node = current_node.get_left()
         return current_node.get_value()
 
-    # a getter method for the minimum element in the tree, this one returns the Node element rather than just the value
+    # a getter method for the minimum element in the tree, this one returns the Node element rather than just the value,
+    # used for the iterator
     def __get_minimum_node(self):
         current_node = self.__root
 
-        # if the root is None, return None
         if current_node is None:
             return None
 
@@ -289,7 +269,6 @@ class BinarySearchTree(object):
     def get_maximum(self):
         current_node = self.__root
 
-        # if the root is None, return None
         if current_node is None:
             return None
 
@@ -298,7 +277,8 @@ class BinarySearchTree(object):
             current_node = current_node.get_right()
         return current_node.get_value()
 
-    # a getter method for the maximum element in the tree, this one returns the Node element rather than just the value
+    # a getter method for the maximum element in the tree, this one returns the Node element rather than just the value,
+    # used for the iterator
     def __get_maximum_node(self):
         current_node = self.__root
 
@@ -312,7 +292,7 @@ class BinarySearchTree(object):
         return current_node
 
 
-# the Node class, which represents a Node for the binary Tree
+# the Node class, which represents a Node in the binary search tree
 class Node(object):
 
     # the constructor for the Node class
@@ -333,11 +313,8 @@ class Node(object):
 
     # a setter method for the left child
     def set_left(self, node=None):
-        # check if the argument's type is Node or None
         if node is None or type(node) == Node:
             self.__left = node
-
-        # if the argument's type is not Node or None, raise a TypeError
         else:
             raise TypeError("The child of a node must be a node.")
 
@@ -347,11 +324,8 @@ class Node(object):
 
     # a setter method for the right child
     def set_right(self, node=None):
-        # check if the argument's type is Node or None
         if node is None or type(node) == Node:
             self.__right = node
-
-        # if the argument's type is not Node or None, raise a TypeError
         else:
             raise TypeError("The child of a node must be a node.")
 
@@ -361,18 +335,14 @@ class Node(object):
 
     # a setter method for the parent of the node
     def set_parent(self, node=None):
-        # check if the argument's type is Node or None
         if node is None or type(node) == Node:
             self.__parent = node
-
-        # if the argument's type is not Node or None, raise a TypeError
         else:
             raise TypeError("The parent of a node must be a node.")
 
     # the add method, which adds a node to the current node as a child,
     # the method returns a boolean representing whether the tree has added an element or not
     def add(self, node):
-        # check if the argument's type is Node
         if type(node) == Node:
             # if the node's value is less than the current node's value, go to the left subtree
             if node.get_value() < self.get_value():
@@ -393,16 +363,12 @@ class Node(object):
             # else if the node's value is equal to the current node's value, do nothing
             else:
                 return False
-
-        # if the argument's type is not Node, raise a TypeError
         else:
             raise TypeError("The node can add only a node as its child.")
 
     # the delete method of the Node class, which deletes a node
     def delete(self, value):
-        # if the current node's value is the value we want to delete, proceed with the delete method
         if self.get_value() == value:
-
             # if we are deleting a node with no children, just delete the node
             if self.__left is None and self.__right is None:
                 if self.__parent is not None:
@@ -486,7 +452,6 @@ class Node(object):
 
     # the contains method, which searches for the value given as an argument
     def contains(self, value):
-        # if the current node's value is equal to the argument value, the element is found, hence return true
         if self.get_value() == value:
             return True
 
@@ -506,21 +471,181 @@ class Node(object):
 
     # the add_left method, which adds a left child to the node and adjust the parent of the node argument
     def __add_left(self, node):
-        # check if the argument's type is Node
         if type(node) == Node:
             self.__left = node
             self.__left.set_parent(self)
-
-        # if the argument's type is not Node, raise a TypeError
         else:
             raise TypeError("You can only add a Node as a left child.")
 
     def __add_right(self, node):
-        # check if the argument's type is Node
         if type(node) == Node:
             self.__right = node
             self.__right.set_parent(self)
-
-        # if the argument's type is not Node, raise a TypeError
         else:
             raise TypeError("You can only add a Node as a right child.")
+
+
+# Abstract Data Type BinaryHeap, abstract class - can't be instantiated, use MinBinaryHeap or MaxBinaryHeap
+class BinaryHeap(ABC):
+
+    # constructor for binary heap, optional argument elements_type (it's set to int if not provided)
+    # the elements in the binary heap must be from the type specified in the constructor,
+    # the default type that is used is int
+    def __init__(self, elements_type=int):
+        self.__elements = []
+        self.__elementsType = elements_type
+
+    # the len(heap) method
+    def __len__(self):
+        return self.size()
+
+    @abstractmethod
+    # overriding the __iter__ method, so that we can go through the elements of the heap
+    def __iter__(self):
+        pass
+
+    # the is_empty method, which checks if the size of the heap is 0
+    def is_empty(self):
+        return len(self.__elements) == 0
+
+    # the size method, which returns the size of the heap
+    def size(self):
+        return len(self.__elements)
+
+    # the type method, which returns the type of the heap elements
+    def type(self):
+        return self.__elementsType
+
+    # the add method, which adds an element to the heap
+    def add(self, element):
+        if type(element) == self.__elementsType:
+            self.__elements.append(element)
+
+            self.__percolate_up()
+        else:
+            raise TypeError("The element you are trying to add is not of type " + str(self.__elementsType))
+
+    # the percolate_up method which adjusts the heap after an addition operation,
+    # it gets the last element in the list and finds its place in the heap
+    @abstractmethod
+    def __percolate_up(self):
+        pass
+
+    @abstractmethod
+    # the percolate_down method, which adjusts the heap after a remove_min operation,
+    # it starts with the first element in the list and finds its place in the heap
+    def __percolate_down(self):
+        pass
+
+    @abstractmethod
+    # the get_sorted_elements method, which returns the sorted elements of the heap
+    def get_sorted_elements(self):
+        pass
+
+    @abstractmethod
+    # the replace_root method, runs faster than remove_min/remove_max followed by add
+    def replace_root(self, element):
+        pass
+
+
+# Abstract Data Type MinBinaryHeap - represents a BinaryHeap with a root its minimum element
+# noinspection PyAbstractClass
+class MinBinaryHeap(BinaryHeap):
+
+    # constructor for MinHeap, same arguments as abstract Heap class
+    def __init__(self, elements_type=None):
+        if elements_type is None:
+            elements_type = int
+        BinaryHeap.__init__(self, elements_type)
+
+        self.__elements = self._BinaryHeap__elements
+        self.__elementsType = self._BinaryHeap__elementsType
+
+    # iterator goes through the sorted elements from
+    def __iter__(self):
+        return iter(self.get_sorted_elements())
+
+    # the percolate_up method which adjusts the heap after an addition operation,
+    # it gets the last element in the list and finds its place in the heap
+    def _BinaryHeap__percolate_up(self):
+        child = len(self.__elements) - 1
+
+        while child > 0:
+            parent = int((child - 1)/2)
+            if self.__elements[child] >= self.__elements[parent]:
+                break
+
+            temp = self.__elements[child]
+            self.__elements[child] = self.__elements[parent]
+            self.__elements[parent] = temp
+            child = parent
+
+    # the percolate_down method, which adjusts the heap after a remove_min operation,
+    #  it gets the first element in the list and finds its place in the heap
+    def _BinaryHeap__percolate_down(self):
+        parent = 0
+        child = 2*parent + 1
+
+        while child < len(self.__elements):
+            if child + 1 < len(self.__elements):
+                if self.__elements[child] > self.__elements[child+1]:
+                    child += 1
+
+            if self.__elements[child] >= self.__elements[parent]:
+                break
+
+            temp = self.__elements[child]
+            self.__elements[child] = self.__elements[parent]
+            self.__elements[parent] = temp
+
+            parent = child
+            child = 2*parent + 1
+
+    # the peek_min method, which returns the minimum element in the heap, but doesn't remove it,
+    # returns None if no elements in the heap
+    def peek_min(self):
+        if len(self.__elements) > 0:
+            return self.__elements[0]
+        else:
+            return None
+
+    # the remove_min method, which removes and returns the minimum element in the heap,
+    # raises a ValueError if there are no elements in the heap
+    def remove_min(self):
+        if len(self.__elements) > 0:
+            min_element = self.__elements[0]
+
+            self.__elements[0] = self.__elements[len(self.__elements) - 1]
+            self.__elements.pop()
+            self._BinaryHeap__percolate_down()
+
+            return min_element
+        else:
+            raise ValueError("There are no elements in the heap")
+
+    # returns the sorted elements in the heap starting from the minimum entry
+    def get_sorted_elements(self):
+        temp_elements = [k for k in self.__elements]
+        sorted_elements = []
+
+        while len(self.__elements) > 0:
+            sorted_elements.append(self.remove_min())
+
+        self.__elements = temp_elements
+        # noinspection PyAttributeOutsideInit
+        self._BinaryHeap__elements = temp_elements
+
+        return sorted_elements
+
+    # removes and returns the smallest item in the heap and adds the new item, faster than remove_min followed by add
+    def replace_root(self, element):
+        if type(element) == self.__elementsType:
+            if len(self.__elements) > 0:
+                temp = self.__elements[0]
+                self.__elements[0] = element
+                self._BinaryHeap__percolate_down()
+                return temp
+            else:
+                raise ValueError("There are no elements in the heap")
+        else:
+            raise TypeError("The element you are trying to add is not of type " + str(self.__elementsType))
