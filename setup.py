c9943e2d8ae00249b7cfc7ce78ff60396be938f0
Lenovo <nikist97@abv.bg>
2017-10-11 11:25:54 +0100

    setup files

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(
+    name='data-structures-and-algorithms',
+    packages=['ADTs', 'Algorithms'],
+    version='0.1',
+    description='A library, which provides implementations for many data structures and algorithms',
+    author='Nikolay Stanchev',
+    author_email='nikist97.ns@gmail.com',
+    url='https://github.com/nikist97/Python-DataStructuresAndAlgorithms',
+    download_url='https://github.com/nikist97/Python-DataStructuresAndAlgorithms/archive/0.1.tar.gz',
+    keywords=['data structures', 'algorithms', 'pythonic', 'python', 'abstract', 'generic'],
+    license="Apache 2.0",
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Data Structures :: Algorithms',
+    ],
+    python_requires='>=3'
+)
