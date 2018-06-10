f5c0653cf78e213be85aa94c736fd5263221144e
nikist97 <nikist97@abv.bg>
2018-06-10 16:36:04 +0100

    Setup file update

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from distutils.core import setup
 
 
+with open("README.md", "r") as file:
+    long_description = file.read()
+
 setup(
     name='data-structures',
     packages=['DataStructures'],
     version='0.1',
     description='Python-based implementations for many data structures.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Nikolay Stanchev',
     author_email='nikist97.ns@gmail.com',
     url='https://github.com/nikist97/Python-DataStructures',
-    download_url='https://github.com/nikist97/Python-DataStructures/archive/0.1.tar.gz',
     keywords=['data structures', 'data', 'structures', 'python', 'abstract', 'generic', 'graph', 'heap', 'queue', 'stack'],
     license="Apache 2.0",
     classifiers=[
